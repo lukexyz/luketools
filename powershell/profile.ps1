f0fba3dd968ab99314940b33a92e93c5a0407407
Luke Woods <lukedwoods@gmail.com>
2021-11-02 13:15:41 +0000

    add powerlevel10k_modern theme

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,8 +1,9 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt powerlevel10k_modern # using Get-PoshThemes
+# oh-my-posh --init --shell pwsh --config "~/jandedobbeleer.omp.json" | Invoke-Expression
+
 Import-Module -Name Terminal-Icons
 
-cd C:\python
+cd C:\python\lighthouse_nbs
 ls
-
