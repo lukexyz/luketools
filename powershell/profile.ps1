869a9c32bb7e502001f6dab09edbd9d0a47d8820
Luke <lukedwoods@gmail.com>
2022-07-25 10:07:28 +0100

    Update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,6 +1,6 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt powerlevel10k_modern # using Get-PoshThemes
+Set-PoshPrompt Paradox # using Get-PoshThemes
 # oh-my-posh --init --shell pwsh --config "~/jandedobbeleer.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
