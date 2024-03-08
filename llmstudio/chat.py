c83d95afa249383a78a87ef03e8e9746b29f2f48
lukexyz <lukedwoods@gmail.com>
2024-03-08 19:23:34 +0000

    streamlit app working with local model

---
+++ llmstudio/chat.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import streamlit as st
+
+st.title("ChatGPT-like clone")
+
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
+
+
+if "model" not in st.session_state:
+    st.session_state["model"] = "local-model"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
