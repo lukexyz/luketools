35a1806bcd97eb51cd8ce4d4dc0439b9d3d4681d
lukexyz <lukedwoods@gmail.com>
2024-08-05 23:48:12 +0100

    add altair chart to plot counter

--- fasthtml/app_counter.py
+++ fasthtml/app_counter.py
@@ -1,31 +1,83 @@
-from fasthtml import *
-from fasthtml.fastapp import *
+from fasthtml.common import *
+from fh_altair import altair2fasthml, altair_headers
+import numpy as np
+import pandas as pd
+import altair as alt
 
-app = FastHTML(hdrs=[picolink])
+# pip install fh-altair
 
+app = FastHTML(hdrs=[picolink, altair_headers])
 count = 0
+plotdata = []
+
+# Custom CSS for centering
+custom_css = Style("""
+    .center-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 30vh;
+        text-align: center;
+    }
+""")
+
+def generate_chart():
+    pltr = pd.DataFrame({'y': plotdata, 'x': range(len(plotdata))})
+    chart = alt.Chart(pltr).mark_line().encode(x='x', y='y').properties(width=400, height=200)
+    return altair2fasthml(chart)
+
 
 @app.get("/")
 def home():
-    return Title("🎲 Count Demo"), Main(
-        H1("🎲 The Great Counter Demo"),
-        P(f"Count is set to {count}", id="count"),
-        Button("Increment", hx_post="/increment", hx_target="#count", hx_swap="innerHTML"),
-        Button("Decrement", hx_post="/decrement", hx_target="#count", hx_swap="innerHTML")
+    return (
+        custom_css,
+        Title("🎲 Count Demo with Chart"),
+        Main(
+            H1("🎲 The Great Counter Demo", id="header"),
+            cls="center-container"
+        ),
+        Main(
+            P(f"Count is set to {count}", id="count"),
+            P(
+                Button("Increment", hx_post="/increment", hx_target="#count"),
+                Button("Decrement", hx_post="/decrement", hx_target="#count"),
+                cls="button-group"
+            ),
+            Div(id="chart"),
+            cls="center-container"
+        )
     )
 
 @app.post("/increment")
 def increment():
-    print("incrementing")
-    global count
+    global count, plotdata
     count += 1
-    return f"Count is set to {count}"
+    plotdata.append(count+np.random.exponential(1))
+    return (
+        P(f"Count is set to {count}", id="count", hx_swap_oob="true"),
+        Div(
+            generate_chart(),
+            P(f"x = {len(plotdata)}, y = {np.round(plotdata[-1], 4)}"),
+            _id="chart",
+            hx_swap_oob="true"
+        )
+    )
 
 @app.post("/decrement")
 def decrement():
     print("decrementing")
-    global count
+    global count, plotdata
     count -= 1
-    return f"Count is set to {count}"
+    plotdata.append(count-np.random.exponential(1))
+    return (
+        P(f"Count is set to {count}", id="count", hx_swap_oob="true"),
+        Div(
+            generate_chart(),
+            P(f"x = {len(plotdata)}, y = {np.round(plotdata[-1], 4)}"),
+            _id="chart",
+            hx_swap_oob="true"
+        )
+    )
 
 serve()
